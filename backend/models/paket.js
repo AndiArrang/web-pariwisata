341d2c3123bf6d066415f90f41a397e9e6edf91d
AndiArrang <andi.muharram@student.unismuh.ac.id>
2024-07-31 11:42:14 +0800

    add relasi Paket to PaketImages

--- backend/models/paket.js
+++ backend/models/paket.js
@@ -2,6 +2,7 @@ import { DataTypes } from "sequelize";
 import db from "../application/db.js";
 import Penginapan from "./penginapan.js";
 import PaketPenginapan from "./paket_penginapan.js";
+import PaketImages from "./paket_images.js";
 
 const Paket = db.define('pakets', {
   id_paket: {
@@ -29,7 +30,12 @@ const Paket = db.define('pakets', {
   }
 })
 
+// relasi many to many paket - penginapan
 Paket.belongsToMany(Penginapan, {through: PaketPenginapan,foreignKey: 'id_paket'})
 Penginapan.belongsToMany(Paket, {through: PaketPenginapan,foreignKey: 'id_penginapan'})
+
+// relasi one to one paket - paketImages
+Paket.hasOne(PaketImages,{foreignKey: 'id_paket'})
+PaketImages.belongsTo(Paket,{foreignKey: 'id_paket'})
 
 export default Paket
