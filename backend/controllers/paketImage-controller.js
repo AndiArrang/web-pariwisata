ed5a42d716d8aa998d6f0f9e41e0ebc8f835a044
AndiArrang <andi.muharram@student.unismuh.ac.id>
2024-07-31 11:49:45 +0800

    finished upload function

--- backend/controllers/paketImage-controller.js
+++ backend/controllers/paketImage-controller.js
@@ -1,19 +1,27 @@
+import { uploadPaketImage } from "../service/paketImage-service.js"
 
 
 const upload = async (req,res,next) => {
-    // try {
-    //     const result = await uploadPaketImage(req.file,req.params.idPaket)
+    try {
+        const image = {
+            id_paket: req.params.idPaket,
+            url: req.file.path,
+            public_id: req.file.filename
+        }
+        console.log(image)
+        const result = await uploadPaketImage(image)
+        console.log(result)
 
-    //     if(result) {
-    //         res.status(200).json({
-    //             status: 'success',
-    //             message: 'Data added successfully.',
-    //             data: result
-    //         })
-    //     }
-    // } catch (error) {
-    //     next(error)
-    // }
+        if(result) {
+            res.status(200).json({
+                status: 'success',
+                message: 'Data added successfully.',
+                data: result
+            })
+        }
+    } catch (error) {
+        next(error)
+    }
 }
 
 export default {
