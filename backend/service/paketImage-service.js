bf97993098bd7b5b15af11e5abafd20f6e4009e2
AndiArrang <andi.muharram@student.unismuh.ac.id>
2024-07-31 11:46:17 +0800

    membuat fungsi uploadPaketImage

--- backend/service/paketImage-service.js
+++ backend/service/paketImage-service.js
@@ -1,1 +1,17 @@
-// 
+import { createPaketImageValidation } from "../validations/paketImage-validation.js"
+import cloudinary from "../config/cloudinary.js"
+import { ResponseError } from "../error-handler/response-error.js"
+import PaketImages from "../models/paket_images.js"
+
+
+export const uploadPaketImage = async (image) => {
+    const result = createPaketImageValidation.validate(image)
+    if (result.error) {
+        await cloudinary.uploader.destroy(image.public_id);
+        throw new ResponseError(400,result.error.message)
+    }
+
+    return PaketImages.create(result.value)
+    
+}
+
